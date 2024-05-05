26661e4f4855c8c72b5801a1898431ba46199795
Juan Trejo <jtrejo13@utexas.edu>
2024-05-05 17:50:48 -0500

    update memcached

--- memcached/memcached.go
+++ memcached/memcached.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"regexp"
+	"strconv"
+	"time"
 )
 
 // check panics if an error is not nil
@@ -26,26 +29,67 @@ var globalCache = make(map[string][]string)
 func validateRequest(request string) (command string, args []string, valid bool) {
 	log.Printf("Validating request: %s\n", request)
 
-	setRegex := regexp.MustCompile(`^\bset\b\s+(\w+)\s+(\d+)\s+(\d+)\s+(\d+)`)
+	setRegex := regexp.MustCompile(`^\bset\b\s+(\w+)\s+(\d+)\s+(-?\d+)\s+(\d+)`)
+	replaceRegex := regexp.MustCompile(`^\breplace\b\s+(\w+)\s+(\d+)\s+(-?\d+)\s+(\d+)`)
 	getRegex := regexp.MustCompile(`^\bget\b\s+(\w+)`)
 
 	if setRegex.MatchString(request) {
 		return "set", setRegex.FindStringSubmatch(request), true
 	} else if getRegex.MatchString(request) {
 		return "get", getRegex.FindStringSubmatch(request), true
+	} else if replaceRegex.MatchString(request) {
+		return "replace", replaceRegex.FindStringSubmatch(request), true
 	}
 
+	log.Printf("Invalid request: %s\n", request)
 	return "", nil, false
 }
 
-func set(key string, value string, flags string, exptime string, byteCount string) {
-		globalCache[key] = []string{value, flags, exptime, byteCount}
+// set stores a key-value pair in the cache
+// The key is the key to store the value under
+// The value is the value to store
+// The flags are metadata about the value
+// The exptime is the time in seconds until the value expires
+// The byteCount is the number of bytes in the value
+// The allowReplacement flag determines if the key-value pair can be replaced
+// Returns true if the key-value pair was stored successfully
+func set(key string, value string, flags string, exptime string, byteCount string, allowReplacement bool) (bool) {
+
+	// Check if the key already exists and if we are allowed to replace it
+	if _, ok := globalCache[key]; ok && !allowReplacement {
+		log.Printf("Key already exists: %s\n", key)
+		return false
+	}
+
+	if exptime == "0" {
+		exptime = fmt.Sprint(math.MaxInt64)
+		log.Printf("Setting key: %s, with value: %s and no exptime\n", key, value)
+	} else {
+		now := time.Now().Unix()
+		exptimeInt, _ := strconv.ParseInt(exptime, 10, 64)
+		exptime = fmt.Sprint(now + exptimeInt)
+		log.Printf("Setting key: %s, with value: %s and exptime: %s\n", key, value, exptime)
+	}
+
+	
+	globalCache[key] = []string{value, flags, exptime, byteCount}
+
+	return true
 }
 
 func get(key string) []string {
 	if value, ok := globalCache[key]; !ok {
 		return nil
 	} else {
+
+		// Check if the key has expired
+		exptime, _ := strconv.Atoi(value[2])
+		if int64(exptime) < time.Now().Unix() {
+			log.Printf("Key has expired: %s\n", key)
+			delete(globalCache, key)
+			return nil
+		}
+
 		log.Printf("Retrieved value: %s\n", value)
 		return value
 	}
@@ -69,23 +113,38 @@ func handleRequest(conn net.Conn) {
 			key := args[1]
 
 			switch command {
-				case "set":
+				case "set", "replace":
+					log.Printf("Received %s request for key: %s\n", command, key)
 
 					flags := args[2]
 					exptime := args[3]
 					byteCount := args[4]
 
+					allowReplacement := command == "replace"
+
 					// Read the next line of the request
 					scanner.Scan()
 					value := scanner.Text()
 
-					set(key, value, flags, exptime, byteCount)
+					if command == "replace" {
+						if _, ok := globalCache[key]; !ok {
+							conn.Write([]byte("NOT_STORED\r\n"))
+							continue
+						}
+					}
+
+					if ok := set(key, value, flags, exptime, byteCount, allowReplacement); !ok {
+						conn.Write([]byte("NOT_STORED\r\n"))
+						continue
+					}
 
 					log.Printf("Stored value: %s\n", value)
 
 					// Send a response to the client
 					conn.Write([]byte("STORED\r\n"))
 				case "get":
+					log.Printf("Received get request for key: %s\n", key)
+
 					// Get the value from the cache
 					result := get(key)
 
@@ -102,7 +161,6 @@ func handleRequest(conn net.Conn) {
 					log.Printf("Invalid command: %s\n", command)
 			}
 		}
-
 	}
 }
 
